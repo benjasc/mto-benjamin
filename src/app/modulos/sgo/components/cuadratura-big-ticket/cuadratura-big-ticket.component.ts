4d7bf9f35ac53fd2dcb20ab7a173215eb8c62121
Andreina Maria Palmera Zambrano <apalmera@aligare.cl>
2019-03-21 15:19:01 -0300

    cuadratura con tabla

--- src/app/modulos/sgo/components/cuadratura-big-ticket/cuadratura-big-ticket.component.ts
+++ src/app/modulos/sgo/components/cuadratura-big-ticket/cuadratura-big-ticket.component.ts
@@ -1,15 +1,22 @@
 import { Component, OnInit } from '@angular/core';
 import { IMyDateModel } from 'mydatepicker';
+import { Session } from 'protractor';
+
 @Component({
   selector: 'app-cuadratura-big-ticket',
   templateUrl: './cuadratura-big-ticket.component.html',
   styleUrls: ['./cuadratura-big-ticket.component.scss']
 })
+
+
+
 export class CuadraturaBigTicketComponent implements OnInit {
-
-
   fecha:String;
-  constructor() { }
+  titulo:string;
+  constructor() {
+    this.titulo = "Consulta de Cuadratura BigTicket - BackOffice";
+    sessionStorage.setItem("titulo",this.titulo);
+   }
 
   ngOnInit() {
   }
